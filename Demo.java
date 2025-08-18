2938c9063f76c440011da0d064b0c6ea11bd1c19
sachin3059 <sachin54077kumar@gmail.com>
2025-08-18 21:24:40 +0530

    interfaces in java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,39 @@
-abstract class A{
-    public abstract void show();
+interface A{
+    int age = 4;     // these variable are final and static
+    String area = "mumbai";
+
+    void show();
+    void config();
+}
+
+// important notes:
+// interface is not a abstract class
+// by default every method inside the interface have abstract method whether you metioned or not
+// interface are generally used in development mode.
+// all variable inside the interface by default are static and final.
+
+// a class can also implements multimple interfaces
+
+interface X{
+    void run();
+}
+
+
+interface Y extends X{
+    void temp();
+}
+
+class B implements A, X{
+    public void show(){
+        System.out.println("in B show");
+    }
+
+    public void config(){
+        System.out.println("in B config");
+    }
+    public void run(){
+        System.out.println("in B run");
+    }
 }
 
 
@@ -7,11 +41,16 @@ public class Demo {
     public static void main(String[] args){
         // we are creating the object of anonymous inner class.
 
-        A obj = new A(){
-            public void show(){
-                System.out.println("in new show");
-            }
-        };
+        B obj = new B();
         obj.show();
+        obj.config();
+
+        System.out.println(A.age); // we can acces the variable of interface without creating any object .
     }
 }
+
+
+
+// class - class => extends
+// class - interface => implements
+// interface - interface => extends
