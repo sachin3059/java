75f0afdd0253967cdc0db7527123a63da33133dd
sachin3059 <sachin54077kumar@gmail.com>
2025-08-18 21:46:37 +0530

    enum in java

--- Demo.java
+++ Demo.java
@@ -1,60 +1,20 @@
-interface A{
-    int age = 4;     // these variable are final and static
-    String area = "mumbai";
-
-    void show();
-    void config();
+enum Status{
+    Running, Failed, Pending, Success;  // named constants
 }
-
-// important notes:
-// interface is not a abstract class
-// by default every method inside the interface have abstract method whether you metioned or not
-// interface are generally used in development mode.
-// all variable inside the interface by default are static and final.
-
-// a class can also implements multimple interfaces
-
-interface X{
-    void run();
-}
-
-
-interface Y extends X{
-    void temp();
-}
-
-class B implements A, X{
-    public void show(){
-        System.out.println("in B show");
-    }
-
-    public void config(){
-        System.out.println("in B config");
-    }
-    public void run(){
-        System.out.println("in B run");
-    }
-}
-
 
 public class Demo {
+
     public static void main(String[] args){
-        // we are creating the object of anonymous inner class.
+        Status s = Status.Running;
+        System.out.println(s);
 
-        A obj = new B();
-        obj.show();
-        obj.config();
+        Status s1 = Status.Failed;
+        System.out.println(s1.ordinal()); // gives 1 order of named constants
+        
 
-        System.out.println(A.age); // we can acces the variable of interface without creating any object .
-
-
-        X obj1 = new B();
-        obj1.run();
+        Status[] s2 = Status.values(); // gives all object of Status class , status.values gives array
+        for(Status ss: s2){
+            System.out.println(ss);
+        }
     }
 }
-
-
-
-// class - class => extends
-// class - interface => implements
-// interface - interface => extends
