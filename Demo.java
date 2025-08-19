ca60a76112532c9bedb0537220ec3f60120e06b4
sachin3059 <sachin54077kumar@gmail.com>
2025-08-19 16:46:38 +0530

    lambda expression

--- Demo.java
+++ Demo.java
@@ -1,5 +1,9 @@
 
 @FunctionalInterface
+interface Calculator{
+    int operation(int a, int b);
+}
+
 interface A{
     void show();
 }
@@ -10,12 +14,17 @@ interface A{
 public class Demo {
 
     public static void main(String[] args){
-      A obj = new A(){
-        public void show(){
-            System.out.println("in show");
-        }
-      };
 
-      obj.show();
+        // lambda expression
+
+        Calculator add = (a, b) -> a + b;
+        Calculator multiply = (a, b) -> a * b;
+
+        System.out.println("Sum: " + add.operation(5, 6));
+        System.out.println("Multiply: " + multiply.operation(5, 6));
+
+
+        A obj = () -> System.out.println("inside lambda function");
+        obj.show();
     }
 }
