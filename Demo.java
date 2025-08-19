a4c8e8fa1eacf2affc0323960a1fce20b55006da
sachin3059 <sachin54077kumar@gmail.com>
2025-08-19 16:36:42 +0530

    functional interface

--- Demo.java
+++ Demo.java
@@ -1,20 +1,21 @@
-enum Status{
-    Running, Failed, Pending, Success;  // named constants
+
+@FunctionalInterface
+interface A{
+    void show();
 }
+
+// in functional interface you can only one abstract method inside interface
+
 
 public class Demo {
 
     public static void main(String[] args){
-        Status s = Status.Running;
-        System.out.println(s);
-
-        Status s1 = Status.Failed;
-        System.out.println(s1.ordinal()); // gives 1 order of named constants
-        
-
-        Status[] s2 = Status.values(); // gives all object of Status class , status.values gives array
-        for(Status ss: s2){
-            System.out.println(ss);
+      A obj = new A(){
+        public void show(){
+            System.out.println("in show");
         }
+      };
+
+      obj.show();
     }
 }
