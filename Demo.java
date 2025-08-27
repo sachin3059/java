eab77602c3c1be77e95a787ab696e7bdfc13df9e
sachin3059 <sachin54077kumar@gmail.com>
2025-08-27 23:02:29 +0530

    done with threads in java

--- Demo.java
+++ Demo.java
@@ -1,30 +1,28 @@
-
-@FunctionalInterface
-interface Calculator{
-    int operation(int a, int b);
-}
-
-interface A{
-    void show();
-}
-
-// in functional interface you can only one abstract method inside interface
-
-
-public class Demo {
-
-    public static void main(String[] args){
-
-        // lambda expression
-
-        Calculator add = (a, b) -> a + b;
-        Calculator multiply = (a, b) -> a * b;
-
-        System.out.println("Sum: " + add.operation(5, 6));
-        System.out.println("Multiply: " + multiply.operation(5, 6));
-
-
-        A obj = () -> System.out.println("inside lambda function");
-        obj.show();
+class A extends Thread{
+    public void run(){
+        for(int i = 1; i <= 100; i++){
+            System.out.println("hi");
+        }
     }
 }
+
+class B extends Thread{
+    public void run(){
+        for(int i = 1; i <= 100; i++){
+            System.out.println("hello");
+        }
+    }
+}
+
+class Demo{
+    public static void main(String[] args){
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
+
+    }
+}
+
+// threads in java
