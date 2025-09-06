e8a1a2a6d4375331059bdc83aa1e65d2a907e580
sachin3059 <sachin54077kumar@gmail.com>
2025-09-06 22:07:19 +0530

    some example of stream api

--- Demo.java
+++ Demo.java
@@ -3,29 +3,20 @@ import java.util.stream.Collectors;
 
 public class Demo{
     public static void main(String[] args){
-        List<String> names = Arrays.asList("John", "Alice", "Bob", "David", "Aman");
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        int sumOfSquares = numbers.stream()
+                            .filter(n -> n%2 == 0)
+                            .map(n -> n * n)
+                            .reduce(0, Integer :: sum);
+
+        System.out.println(sumOfSquares);
 
 
-        List<String> res = new ArrayList<>();
-        for(String name: names){
-            if(name.startsWith("A")){
-                res.add(name.toUpperCase());
-            }
-        }
+        // notes: 
+        
+        // stream is basically an interface
+        // you can't use stream again
 
-        System.out.println(res);
-
-
-        // stream api => is used for processing collection of data(like arrays, list, sets or maps) in a declartive way
-        // such as filtering, mapping, reducing, etc.
-
-        List<String> students = Arrays.asList("Abhinash", "Lokendra", "Gulshan", "Adarsh", "Ankur", "Sachin");
-
-        List<String> temp = students.stream()
-                                            .filter(student -> student.startsWith("A"))
-                                            .map(String:: toUpperCase)
-                                            .collect(Collectors.toList());
-
-        System.out.println(temp);
     }
 }
