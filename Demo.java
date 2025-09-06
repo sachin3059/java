edcc824c3f3f71626458d68c3d73a794063ff4b9
sachin3059 <sachin54077kumar@gmail.com>
2025-09-06 22:03:11 +0530

    stream api in java

--- Demo.java
+++ Demo.java
@@ -1,26 +1,31 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Demo{
     public static void main(String[] args){
-        Map<Integer, String> map = new HashMap<>();
+        List<String> names = Arrays.asList("John", "Alice", "Bob", "David", "Aman");
 
-        // Adding key-value pairs
-        map.put(1, "Alice");
-        map.put(2, "Bob");
-        map.put(3, "Charlie");
-        map.put(2, "Bobby"); // key already exists → value updated
 
-        // Accessing
-        System.out.println(map.get(2)); // Bobby
-        System.out.println(map.containsKey(3)); // true
-        System.out.println(map.containsValue("Charlie")); // true
-
-        // Removing
-        map.remove(1);
-
-        // Iterating
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " → " + entry.getValue());
+        List<String> res = new ArrayList<>();
+        for(String name: names){
+            if(name.startsWith("A")){
+                res.add(name.toUpperCase());
+            }
         }
+
+        System.out.println(res);
+
+
+        // stream api => is used for processing collection of data(like arrays, list, sets or maps) in a declartive way
+        // such as filtering, mapping, reducing, etc.
+
+        List<String> students = Arrays.asList("Abhinash", "Lokendra", "Gulshan", "Adarsh", "Ankur", "Sachin");
+
+        List<String> temp = students.stream()
+                                            .filter(student -> student.startsWith("A"))
+                                            .map(String:: toUpperCase)
+                                            .collect(Collectors.toList());
+
+        System.out.println(temp);
     }
 }
