dc3199a3025eac895f823280f279137646c1565a
sachin3059 <sachin54077kumar@gmail.com>
2025-09-26 13:23:00 +0530

    Autowired annotation in java

--- SpringBoot/src/main/java/com/example/SpringBoot/Application.java
+++ SpringBoot/src/main/java/com/example/SpringBoot/Application.java
@@ -2,12 +2,20 @@ package com.example.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+     ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        Alien a1 = context.getBean(Alien.class);
+        a1.code();
+
+        Alien a2 = context.getBean(Alien.class);
+        a2.code();
 	}
 
 }
