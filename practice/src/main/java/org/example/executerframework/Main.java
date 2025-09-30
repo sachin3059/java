5569caa069f80a4f23370cca77191bc91d739dc4
sachin3059 <sachin54077kumar@gmail.com>
2025-09-30 23:27:11 +0530

    scheduled executer service

--- practice/src/main/java/org/example/executerframework/Main.java
+++ practice/src/main/java/org/example/executerframework/Main.java
@@ -1,43 +1,14 @@
 package org.example.executerframework;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        long startTime = System.currentTimeMillis();
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        for(int i = 1; i < 10; i++){
-            int finalI = i;
-            executor.submit(() -> {
-                long result = factorial(finalI);
-                System.out.println(result);
-            });
-        }
-        executor.shutdown();
-
-        try {
-            executor.awaitTermination(6, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Total time: " + (System.currentTimeMillis() - startTime));
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.schedule(() -> System.out.println("Task executed after 5 secs delay"), 5, TimeUnit.SECONDS);
+        scheduler.shutdown();
     }
 
-
-    private static long factorial(int n){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        long res = 1;
-        for(int i = 2; i <= n; i++){
-            res *= i;
-        }
-        return res;
-    }
 }
