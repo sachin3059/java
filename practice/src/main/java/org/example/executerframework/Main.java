083c814efb95f8decf7557ad750076061c81b2c1
sachin3059 <sachin54077kumar@gmail.com>
2025-09-30 23:56:26 +0530

    CountDownLatch

--- practice/src/main/java/org/example/executerframework/Main.java
+++ practice/src/main/java/org/example/executerframework/Main.java
@@ -1,14 +1,45 @@
 package org.example.executerframework;
 
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.schedule(() -> System.out.println("Task executed after 5 secs delay"), 5, TimeUnit.SECONDS);
-        scheduler.shutdown();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        int numberOfServices = 3;
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        CountDownLatch latch = new CountDownLatch(numberOfServices);
+        // CountDownLatch is used when one or more threads need to wait until a set of operations in other threads completes.
+
+
+        executorService.submit(new DependentService(latch));
+        executorService.submit(new DependentService(latch));
+        executorService.submit(new DependentService(latch));
+        latch.await();
+
+        System.out.println("Main");
+        executorService.shutdown();
+
+
+
+
+    }
+}
+
+
+class  DependentService implements Callable<String> {
+    private final CountDownLatch latch;
+
+    public DependentService(CountDownLatch latch){
+        this.latch = latch;
     }
 
+    @Override
+    public String call() throws  Exception {
+        System.out.println(Thread.currentThread().getName() + "Service started");
+        Thread.sleep(2000);
+        latch.countDown();
+        return  "ok";
+    }
 }
