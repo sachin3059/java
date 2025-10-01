27085112013a2977986812ac82c37942fc315dc0
sachin3059 <sachin54077kumar@gmail.com>
2025-10-02 01:30:03 +0530

    basic bean setup using application context and xml configuration

---
+++ SpringFrameworkCore/src/main/java/edu/example/MyBean.java
@@ -0,0 +1,23 @@
+package edu.example;
+
+
+
+public class MyBean {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void showMessage(){
+        System.out.println("Message: " + message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
