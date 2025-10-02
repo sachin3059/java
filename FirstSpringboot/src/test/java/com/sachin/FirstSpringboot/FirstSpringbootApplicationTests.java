e5714b9fc1e5e0282984acf7f528dab7fcdadf65
sachin3059 <sachin54077kumar@gmail.com>
2025-10-03 01:58:33 +0530

    Rest API

---
+++ FirstSpringboot/src/test/java/com/sachin/FirstSpringboot/FirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin.FirstSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
