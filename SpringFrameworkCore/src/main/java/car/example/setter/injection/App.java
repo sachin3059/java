0ae1f4e37dd22e7397e21e739f17269e1ac54680
sachin3059 <sachin54077kumar@gmail.com>
2025-10-02 22:18:28 +0530

    autoWiring with xml using "byName"

--- SpringFrameworkCore/src/main/java/car/example/setter/injection/App.java
+++ SpringFrameworkCore/src/main/java/car/example/setter/injection/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.SetterInjection.xml");
 
-        Car car = (Car) context.getBean("myNewCar");
-        car.displayDetails();
+        Car myNewCar = (Car) context.getBean("myNewCar");
+        myNewCar.displayDetails();
     }
 }
